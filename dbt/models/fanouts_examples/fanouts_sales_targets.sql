7515556309a3c12a7cd8b1e27e4c631ac075b4a2
Lightdash <developers@lightdash.com>
2026-05-26 21:16:30 +0000

    Add tiger_goal dimension and tiger_goal_sum metric to fanouts_sales_targets

--- dbt/models/fanouts_examples/fanouts_sales_targets.sql
+++ dbt/models/fanouts_examples/fanouts_sales_targets.sql
@@ -1,9 +1,10 @@
-SELECT 
+SELECT
     target_type,
     target_value,
     quarter_start_date,
     {{ quarter_end_date('quarter_start_date') }} AS quarter_end_date,
     target_deals,
-    target_amount
+    target_amount,
+    target_amount * 1.5 AS tiger_goal
 FROM {{ ref('sales_targets_raw') }}
    
